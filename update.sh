40d302db4f1cd2319f47f97d1ab94e47f3a30d6f
Andrea Musuruane <musuruan@gmail.com>
2022-12-24 10:50:06 +0100

    Rename *.oql to *.overpassql (Issue #1)

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # https://overpass-api.de/command_line.html
-wget -O "./results/municipi.osm" --post-file="./queries/municipi.oql" "https://overpass-api.de/api/interpreter"
-wget -O "./results/uffici_postali.osm" --post-file="./queries/uffici_postali.oql" "https://overpass-api.de/api/interpreter"
-wget -O "./results/chiese.osm" --post-file="./queries/chiese.oql" "https://overpass-api.de/api/interpreter"
-wget -O "./results/fontanelle.osm" --post-file="./queries/fontanelle.oql" "https://overpass-api.de/api/interpreter"
-wget -O "./results/parchi_giochi.osm" --post-file="./queries/parchi_giochi.oql" "https://overpass-api.de/api/interpreter"
+wget -O "./results/municipi.osm" --post-file="./queries/municipi.overpassql" "https://overpass-api.de/api/interpreter"
+wget -O "./results/uffici_postali.osm" --post-file="./queries/uffici_postali.overpassql" "https://overpass-api.de/api/interpreter"
+wget -O "./results/chiese.osm" --post-file="./queries/chiese.overpassql" "https://overpass-api.de/api/interpreter"
+wget -O "./results/fontanelle.osm" --post-file="./queries/fontanelle.overpassql" "https://overpass-api.de/api/interpreter"
+wget -O "./results/parchi_giochi.osm" --post-file="./queries/parchi_giochi.overpassql" "https://overpass-api.de/api/interpreter"
