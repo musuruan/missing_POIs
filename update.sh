5f4c58e692d3b14b7eca30c5a024f9cafc64d4f2
Andrea Musuruane <musuruan@gmail.com>
2025-04-06 21:40:46 +0200

    Aggiunti tabaccai e scuole

--- update.sh
+++ update.sh
@@ -6,3 +6,6 @@ wget -O "./results/uffici_postali.osm" --post-file="./queries/uffici_postali.ove
 wget -O "./results/chiese.osm" --post-file="./queries/chiese.overpassql" "https://overpass-api.de/api/interpreter"
 wget -O "./results/fontanelle.osm" --post-file="./queries/fontanelle.overpassql" "https://overpass-api.de/api/interpreter"
 wget -O "./results/parchi_giochi.osm" --post-file="./queries/parchi_giochi.overpassql" "https://overpass-api.de/api/interpreter"
+wget -O "./results/tabaccai.osm" --post-file="./queries/tabaccai.overpassql" "https://overpass-api.de/api/interpreter"
+wget -O "./results/scuole.osm" --post-file="./queries/scuole.overpassql" "https://overpass-api.de/api/interpreter"
+
